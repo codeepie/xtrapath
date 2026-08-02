4dca0fe8c0e742383fe02c5e477eb91cf125d7e1
yogendra singh <yogendra20799@gmail.com>
2026-08-02 23:18:15 +0530

    fix: Add PangoCairo dependencies to build script

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-echo "Installing system dependencies (ffmpeg, texlive)..."
-apt-get update && apt-get install -y ffmpeg texlive-latex-extra
+echo "Installing system dependencies (ffmpeg, texlive, pango, cairo)..."
+apt-get update && apt-get install -y ffmpeg texlive-latex-extra pkg-config libcairo2-dev libpango1.0-dev
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
